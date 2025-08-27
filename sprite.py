dca06b9ddf2ea9ca7bb38d5c78f874de5926c4d0
Aaren Hess <aarenhess@gmail.com>
2025-08-26 20:31:10 -0400

    Player Ship sprite Updated

--- sprite.py
+++ sprite.py
@@ -10,9 +10,10 @@ class Sprite():
         self.image = pygame.image.load(imagePath).convert_alpha()
         imageWidth = self.image.get_width()
         
-        for i in range(imageWidth//int(self.width)):
+        for i in range(0,imageWidth//int(self.width),1):
             frame_rect = pygame.Rect(i * self.width,0,self.width,self.height)
             self.frames.append(self.image.subsurface(frame_rect))
+            
         self.frameAmt = len(self.frames) - 1
         self.imageSurface = pygame.Surface([width, height], pygame.SRCALPHA)
         self.imageSurface.blit(self.frames[0],(0,0))
@@ -21,12 +22,13 @@ class Sprite():
         screen.blit(self.imageSurface,position)
         self.updateAnimation()
 
-    def updateAnimation(self): #Not Used, for now
+    def updateAnimation(self):
         self.framerateTimer += 1
         if self.framerateTimer >= 6:
             self.framerateTimer = 0
             self.imageSurface.blit(self.frames[self.currentFrame],(0,0))
             self.currentFrame += 1
-            if self.currentFrame >= len(self.frames) - 1:
+            if self.currentFrame > len(self.frames) - 1:
                 self.currentFrame = 0
+            
                 
